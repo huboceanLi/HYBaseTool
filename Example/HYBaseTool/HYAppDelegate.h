db72446008c38a1202f2b55bdf0f22f10e1505cb
oceanLi <li437277219@gmail.com>
2023-03-31 09:56:27 +0800

    Initial commit

---
+++ Example/HYBaseTool/HYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HYAppDelegate.h
+//  HYBaseTool
+//
+//  Created by li437277219@gmail.com on 03/31/2023.
+//  Copyright (c) 2023 li437277219@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/HYBaseTool/HYViewController.h
@@ -0,0 +1,13 @@
+//
+//  HYViewController.h
+//  HYBaseTool
+//
+//  Created by li437277219@gmail.com on 03/31/2023.
+//  Copyright (c) 2023 li437277219@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HYViewController : UIViewController
+
+@end
